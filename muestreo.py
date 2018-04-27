ae9d260a65b13258355c8a3b35dea3c7532df176
Conkertafolla <30909174+Conkertafolla@users.noreply.github.com>
2018-04-26 20:48:56 -0500

    Empezando hacer la parte de muestreo

---
+++ muestreo.py
@@ -0,0 +1,8 @@
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+def generaDatos():
+    
