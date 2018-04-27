9b08380c9a07e20d46899f8c542d272c39d55675
Conkertafolla <30909174+Conkertafolla@users.noreply.github.com>
2018-04-26 23:33:59 -0500

    aun no salu u.u

--- muestreo.py
+++ muestreo.py
@@ -7,23 +7,34 @@ from matplotlib.pyplot import show
 
 #librerias numpy
 import numpy as np
-from numpy import arange, sin, pi
+from numpy import arange, sin, pi,floor
 
 amplitud=1
 frecuencia=1
-periodo=arange(0.0, 2*pi, 0.01)
+fase=0
+muestras=0
+periodo=arange(0.0, 2*pi, 0.001)
 
-#Funcion para generar los datos a Graficar
+#Funcion para generar los datos a Graficar de la funcion original
 def datosSenal(amplitud,frecuencia,fase):
     return np.sin(((periodo)*frecuencia)+fase)*amplitud
 
+#funion para generar los datos a graficar de la funcion muestreada
+def muestreo(amplitud,frecuencia,fase,muestras):
+    t=1/muestras
+    n=arange(0,2*pi)
+
+    return arange(n*t,np.sin(((periodo)*frecuencia*t)+fase)*amplitud)
+
+
+
 #funcion para graficar
-def grafica (amplitud,frecuencia,fase):
+def grafica (amplitud,frecuencia,fase,muestras):
     f=Figure()
-    ax1=f.add_subplot(211)
-    ax1.plot(periodo, datosSenal(amplitud,frecuencia,fase))
-    ax2=f.add_subplot(212)
-    ax2.plot(periodo, datosSenal(amplitud-2,frecuencia,fase))
+    ax=f.add_subplot(211)
+    plt.plot(periodo, datosSenal(amplitud,frecuencia,fase))
+    ax=f.add_subplot(212)
+    plt.plot(periodo, muestreo(amplitud,frecuencia,fase,muestras))
     show()
 
 
@@ -32,4 +43,5 @@ def grafica (amplitud,frecuencia,fase):
 amplitud=int(input("\nIntroducir la amplitud de la señal: "))
 frecuencia=int(input("\nFrecuencia de la señal"))
 fase=int(input("\nFase de la señal"))
-grafica(amplitud,frecuencia,fase)
+muestras=int(input("\nFrecuencia de muestreo"))
+grafica(amplitud,frecuencia,fase,muestras)
